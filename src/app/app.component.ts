42341c13321eece1958d70568b12167a40dc9464
Ventos Company <exceptiondata@gmail.com>
2019-05-31 17:45:45 +0430

    fcm publish token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,47 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { FCM,NotificationData } from '@ionic-native/fcm';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private fcm:FCM) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.fcm.getToken().then(
+        (token: string)=>{
+          console.log("getToken is : "+token);
+        }
+      ).catch(error=>{
+        console.log(error);
+      });  
+
+      this.fcm.onTokenRefresh().subscribe((token: string) =>{
+        console.log("initialize token : "+token);
+      });
+
+      this.fcm.onNotification().subscribe(data => {
+
+        if(data.wasTapped)
+        {
+          console.log("data Wastapped: "+JSON.stringify(data));
+        }
+        else
+        {
+            console.log("data Dont Wastapped: "+JSON.stringify(data));
+        }
+
+      },error =>{
+        console.log("error Onnotification : "+error);
+      });
+
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FCM } from '@ionic-native/fcm';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    FCM,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
